bede553975f14da3d9f8c9328eb0d6e9f94e6b46
Anca.Sin <Anca.Sin@gmail.com>
2025-02-22 00:10:08 +0100

    Implement list_all_habits() in Analytics class

--- corpus_meta_lunae/analytics.py
+++ corpus_meta_lunae/analytics.py
@@ -1,6 +1,7 @@
 from .user import User
 from .habit import Habit
 from .streaks import Streaks
+from typing import List
 
 # "The functionality of this analytics module must be implemented using the functional programming paradigm."
 class Analytics:
@@ -16,7 +17,17 @@ class Analytics:
         self.streaks = Streaks() # Create an instance of Streaks to analyze
 
     # Task requirement: "return a list of all currently tracked habits"
-    def list_all_habits(self):
+    def list_all_habits(self) -> List[str]:
+        """
+        Retrieves the current habits of the user.
+
+        :return: List of habit names.
+        """
+        # Using list comprehension
+        # return [habit.name for habit in self.user.habits]
+
+        # Using FPP
+        return list(map(lambda habit: habit.name, self.user.habits))
 
     # Task requirement: "return a list of all habits with the same periodicity"
 
