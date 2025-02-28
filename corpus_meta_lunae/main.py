519b9ef0b2ba87638919bcd90da6592237cc5d55
Anca.Sin <Anca.Sin@gmail.com>
2025-03-01 00:48:43 +0100

    Fix methods in main.py to align to the same cli + Simplify the code (e.g. remove choice.isdigit check everywhere)

--- corpus_meta_lunae/main.py
+++ corpus_meta_lunae/main.py
@@ -52,27 +52,27 @@ class HabitTracker:
         """Displays the main menu and handles user navigation."""
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("""\n- - - Main Menu - - -
+            print("""- - - Main Menu - - -
+            
             1. My Habit Tracker
             2. Select a different user
             3. Delete this user
             4. Exit
             """)
 
-            choice = input("Enter your choice (1-4): ").strip()
+            choice = input("\nEnter your choice (1-4): ").strip()
 
-            if choice.isdigit():
-                if choice == "1":
-                    self.my_habit_tracker()
-                elif choice == "2":
-                    self.logged_in_user = self.select_user()
-                    self.load_user_data()
-                elif choice == "3":
-                    self.db.delete_user()
-                elif choice == "4":
-                    print("Until next time! Do your best to stay on track!")
+            if choice == "1":
+                self.my_habit_tracker()
+            elif choice == "2":
+                self.logged_in_user = self.select_user()
+                self.load_user_data()
+            elif choice == "3":
+                self.db.delete_user()
+            elif choice == "4":
+                print("\nUntil next time! Do your best to stay on track!")
             else:
-                print("Sorry, invalid option. Please try again!")
+                print("\nSorry, invalid input. Please try again!")
                 reload_menu_countdown()
 
     def my_habit_tracker(self):
@@ -88,15 +88,14 @@ class HabitTracker:
 
             choice = input("\nEnter your choice (1-3): ").strip()
 
-            if choice.isdigit():
-                if choice == "1":
-                    self.habits_menu()
-                elif choice == "2":
-                    self.analytics_menu()
-                elif choice == "3":
-                   break # Return to the main_menu's loop
+            if choice == "1":
+                self.habits_menu()
+            elif choice == "2":
+                self.analytics_menu()
+            elif choice == "3":
+                break # Return to the main_menu's loop
             else:
-                print("Sorry, invalid option. Please try again!")
+                print("\nSorry, invalid input. Please try again!")
                 reload_menu_countdown()
 
     def habits_menu(self):
@@ -104,6 +103,7 @@ class HabitTracker:
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
             print("""- - - My Habits - - -
+            
             1. Register new habit
             2. List all habits
             3. Daily habits
@@ -111,21 +111,20 @@ class HabitTracker:
             5. Back to main menu
             """)
 
-            choice = input("Enter your choice (1-5): ").strip()
+            choice = input("\nEnter your choice (1-5): ").strip()
 
-            if choice.isdigit():
-                if choice == "1":
-                    self.register_new_habit()
-                elif choice == "2":
-                    self.list_habits()
-                elif choice == "3":
-                    self.list_daily_habits()
-                elif choice == "4":
-                    self.list_weekly_habits()
-                elif choice == "5":
-                    break # Return to the main_menu's loop
+            if choice == "1":
+                self.register_new_habit()
+            elif choice == "2":
+                self.list_habits()
+            elif choice == "3":
+                self.list_daily_habits()
+            elif choice == "4":
+                self.list_weekly_habits()
+            elif choice == "5":
+                break # Return to the main_menu's loop
             else:
-                print("Sorry, invalid option. Please try again!")
+                print("\nSorry, invalid option. Please try again!")
                 reload_menu_countdown()
 
     def analytics_menu(self):
@@ -135,37 +134,38 @@ class HabitTracker:
 
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("""\n- - - My Analytics - - -
+            print("""- - - My Analytics - - -
+            
             1. Longest streak across all habits
             2. Most completed habit
             3. Least completed habit
             4. Back to main menu
             """)
 
-            choice = input("Enter your choice (1-4): ").strip()
+            choice = input("\nEnter your choice (1-4): ").strip()
 
-            if choice.isdigit():
-                if choice == "1":
-                    analytics.longest_streak_all_habits()
-                elif choice == "2":
-                    analytics.most_completed_habit()
-                elif choice == "3":
-                    analytics.least_completed_habit()
-                elif choice == "4":
-                    break
+            if choice == "1":
+                analytics.longest_streak_all_habits()
+            elif choice == "2":
+                analytics.most_completed_habit()
+            elif choice == "3":
+                analytics.least_completed_habit()
+            elif choice == "4":
+                break
             else:
-                print("Sorry, invalid option. Please try again!")
+                print("\nSorry, invalid option. Please try again!")
                 reload_menu_countdown()
 
     def register_new_habit(self, frequency_preset=None):
         """Guides the user through a new habit creation."""
+        os.system('cls' if os.name == 'nt' else 'clear')
         new_habit = Habit()
         new_habit.habit_name()
 
         if frequency_preset:
             # Use the frequency preset
             new_habit.frequency = frequency_preset
-            print(f"Frequency automatically set to {frequency_preset}.")
+            print(f"\nFrequency automatically set to {frequency_preset}.")
         else:
             # Let the user chose the frequency
             new_habit.habit_frequency()
@@ -177,13 +177,15 @@ class HabitTracker:
 
         # Save to db
         self.db.save_habits(self.logged_in_user)
-        print(f"{new_habit.name} creation process completed!")
+        print(f"\n{new_habit.name} creation process completed!")
+        reload_menu_countdown()
 
     def list_habits(self):
         """
         Lists only daily habits and allows the user to select one for detailed view.
         Uses the analytics module's functional programming methods.
         """
+        os.system('cls' if os.name == 'nt' else 'clear')
         # Get all habits (using Analytics)
         analytics = Analytics(self.logged_in_user)
         all_habits = analytics.list_all_habits()
@@ -199,30 +201,30 @@ class HabitTracker:
         # Display all habits with numbering
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\n- - - My Habits - - -")
+            print("""- - - My Habits - - -
+            
+            """)
             for idx, habit in enumerate(self.logged_in_user.habits, 1): # Start indexing at 1, instead of default 0
                 print(f"{idx}. {habit.name} ({habit.frequency})")
 
-            habits_number = len(all_habits)
             # Add extra option to go back to previous menu at the end
-            back_option = habits_number + 1
-            print(f"{back_option}. Back to My Habits Menu")
+            # Calculate back option indexing according to number of habits
+            habits_number = len(all_habits)
+            back_option_idx = habits_number + 1
+            print(f"{back_option_idx}. Back to my habits menu")
 
             # Get user selection
-            choice = input(f"Enter your choice (1-{back_option}): ")
+            choice = input(f"\nEnter your choice (1-{back_option_idx}): ")
 
-            if choice.isdigit():
-                choice_num = int(choice)
-                if 1 <= choice_num <= habits_number:
-                    # Show detailed manu for selected habit
-                    self.habit_detail_menu(all_habits[choice_num - 1]) # Adjust back to 0 indexing
-                    # Exit the loop after selecting a habit or returning from the detail menu
-                    break
-                elif choice_num == back_option:
-                    break # Return to My Habits menu
-
+            if 1 <= int(choice) <= habits_number:
+                # Show detailed manu for selected habit
+                self.habit_detail_menu(all_habits[int(choice) - 1]) # Adjust back to 0 indexing
+                # Exit the loop after selecting a habit or returning from the detail menu
+                break
+            elif int(choice) == back_option_idx:
+                break # Return to My Habits menu
             else:
-                print("Sorry, invalid input. Please try again!") # Handle invalid input and re-run the loop
+                print("\nSorry, invalid input. Please try again!") # Handle invalid input and re-run the loop
                 reload_menu_countdown()
 
     def list_daily_habits(self):
@@ -242,50 +244,50 @@ class HabitTracker:
                 print("You don't have any daily habits yet!")
                 time.sleep(1)
                 # Ask the user if they want to create a daily habit or return to the previous menu
-                choice = input("""Please chose between 1 or 2:
+                print("""\nPlease chose between option 1 or 2:
+                
                 1. Create a new daily habit
-                2. Back to My Habits menu
-                """).strip()
+                2. Back to my habits menu                
+                """)
 
-                if choice.isdigit():
-                    if choice == "1":
-                        self.register_new_habit(frequency_preset="daily")
-                        return
-                    elif choice == "2":
-                        return  # Return to My Habits menu
+                choice = input(f"\nEnter your choice (1-2): ")
 
+                if choice == "1":
+                    self.register_new_habit(frequency_preset="daily")
+                    return
+                elif choice == "2":
+                    return  # Return to My Habits menu
                 else:
-                    print("Sorry, invalid input. Please try again!")
+                    print("\nSorry, invalid input. Please try again!")
                     reload_menu_countdown()
 
         # Display daily habits with numbering
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\n- - - Daily Habits - - -")
+            print("""- - - Daily Habits - - -
+            
+            """)
             for idx, habit in enumerate(daily_habits, 1): # Start indexing at 1
                 print(f"{idx}. {habit.name}")
 
-            habits_number = len(daily_habits)
             # Add extra option to go back to previous menu at the end
-            back_option = habits_number + 1
-            print(f"{back_option}. Back to My Habits Menu")
+            # Calculate back option indexing according to number of habits
+            habits_number = len(daily_habits)
+            back_option_idx = habits_number + 1
+            print(f"{back_option_idx}. Back to my habits menu")
 
             # Get user selection
-            choice = input(f"Enter your choice (1-{back_option}): ")
+            choice = input(f"\nEnter your choice (1-{back_option_idx}): ")
 
-            if choice.isdigit():
-                choice_num = int(choice)
-
-                if 1 <= choice_num <= habits_number:
-                    # Show detailed manu for selected habit
-                    self.habit_detail_menu(daily_habits[choice_num - 1]) # Adjust back to 0 indexing
-                    # Exit the loop after selecting a habit or returning from the detail menu
-                    break
-                elif choice_num == back_option:
-                    break # Return to My Habits menu
-
+            if 1 <= int(choice) <= habits_number:
+                # Show detailed manu for selected habit
+                self.habit_detail_menu(daily_habits[int(choice) - 1]) # Adjust back to 0 indexing
+                # Exit the loop after selecting a habit or returning from the detail menu
+                break
+            elif int(choice) == back_option_idx:
+                break # Return to My Habits menu
             else:
-                print("Sorry, invalid input. Please try again!") # Handle general invalid input and re-run the loop
+                print("\nSorry, invalid input. Please try again!")
                 reload_menu_countdown()
 
     def list_weekly_habits(self):
@@ -305,18 +307,18 @@ class HabitTracker:
                 print("You don't have any weekly habits yet!")
                 time.sleep(1)
                 # Ask the user if they want to create a weekly habit or return to the previous menu
-                choice = input("""Please chose between 1 or 2:
+                choice = input("""\nPlease chose between 1 or 2:
+                
                 1. Create a new weekly habit
-                2. Back to My Habits menu
+                2. Back to my habits menu
+                
                 """).strip()
 
-                if choice.isdigit():
-                    if choice == "1":
-                        self.register_new_habit(frequency_preset="weekly")
-                        return
-                    elif choice == "2":
-                        return  # Return to My Habits menu
-
+                if choice == "1":
+                    self.register_new_habit(frequency_preset="weekly")
+                    return
+                elif choice == "2":
+                    return  # Return to My Habits menu
                 else:
                     print("Sorry, invalid input. Please try again!")
                     reload_menu_countdown()
@@ -324,58 +326,54 @@ class HabitTracker:
         # Display weekly habits with numbering
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\n- - - Weekly Habits - - -")
+            print("""- - - Weekly Habits - - -
+            
+            """)
             for idx, habit in enumerate(weekly_habits, 1):  # Start indexing at 1
                 print(f"{idx}. {habit.name}")
 
-            habits_number = len(weekly_habits)
             # Add extra option to go back to previous menu at the end
-            back_option = habits_number + 1
-            print(f"{back_option}. Back to My Habits Menu")
+            # Calculate back option indexing according to number of habits
+            habits_number = len(weekly_habits)
+            back_option_idx = habits_number + 1
+            print(f"{back_option_idx}. Back to my habits menu")
 
             # Get user selection
-            choice = input(f"Enter your choice (1-{back_option}): ")
+            choice = input(f"\nEnter your choice (1-{back_option_idx}): ")
 
-            if choice.isdigit():
-                choice_num = int(choice)
-
-                if 1 <= choice_num <= habits_number:
-                    # Show detailed manu for selected habit
-                    self.habit_detail_menu(weekly_habits[choice_num - 1])  # Adjust back to 0 indexing
-                    # Exit the loop after selecting a habit or returning from the detail menu
-                    break
-                elif choice_num == back_option:
-                    break  # Return to My Habits menu
-                else:
-                    print(f"Please enter a number between 1 and {back_option}!")  # Handle invalid int input
-                    reload_menu_countdown()
+            if 1 <= int(choice) <= habits_number:
+                # Show detailed manu for selected habit
+                self.habit_detail_menu(weekly_habits[int(choice) - 1])  # Adjust back to 0 indexing
+                # Exit the loop after selecting a habit or returning from the detail menu
+                break
+            elif int(choice) == back_option_idx:
+                break  # Return to My Habits menu
             else:
-                print("Sorry, invalid input. Please try again!")  # Handle general invalid input and re-run the loop
+                print("\nSorry, invalid input. Please try again!")  # Handle general invalid input and re-run the loop
                 reload_menu_countdown()
 
     def habit_detail_menu(self, habit):
         """Shows detail menu for a selected habit and allows for completion/analysis."""
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"""\n- - - {habit.name}'s Details - - -
+            print(f"""- - - {habit.name}'s Details - - -
+            
             1. Completion
-            2. Delete a completion
+            2. Delete completion
             3. {habit.name}'s Analytics
             4. Back            
             """)
 
-            choice = input("Enter your choice (1-4): ").strip()
+            choice = input("\nEnter your choice (1-4): ").strip()
 
-            if choice.isdigit():
-                if choice == "1":
-                    self.complete_habit(habit)
-                if choice == "2":
-                    self.view_habit_analytics(habit)
-                elif choice == "3":
-                    break
-
+            if choice == "1":
+                self.complete_habit(habit)
+            elif choice == "2":
+                self.view_habit_analytics(habit)
+            elif choice == "3":
+                break
             else:
-                print("Sorry, invalid input. Please try again!")
+                print("\nSorry, invalid input. Please try again!")
                 reload_menu_countdown()
 
     def complete_habit(self):
