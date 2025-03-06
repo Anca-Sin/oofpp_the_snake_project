ce1d3631e1f660badb2f82b33381311cfde3bfd4
Anca.Sin <Anca.Sin@gmail.com>
2025-03-06 20:55:43 +0100

    Add documentation to config

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
+"""
+Configuration module for the habit tracker application.
+This module defines global config variables and functions to modify them.
+"""
+
+# Defaults database filepath
 DB_FILEPATH = "habit_tracker.db"
 
-def set_db_filepath(filepath):
+def set_db_filepath(filepath: str):
+    """
+    Sets the global db filepath.
+
+    - allows changing the db filepath at runtime, which is useful for testing or when specified by the user
+
+    Args:
+        filepath: The new db filepath to use.
+    """
     global DB_FILEPATH
     DB_FILEPATH = filepath
