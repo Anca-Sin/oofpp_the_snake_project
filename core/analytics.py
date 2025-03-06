2dde58e25faaa0c9d76550791a961b3ae63b7d89
Anca.Sin <Anca.Sin@gmail.com>
2025-03-06 17:44:29 +0100

    Review Core - Analytics for logic and documentation

--- core/analytics.py
+++ core/analytics.py
@@ -1,48 +1,56 @@
+from typing import List, Tuple
+
 from .user import User
 from .habit import Habit
-from typing import List
-
-# Task requirement:
-# "The functionality of this analytics module must be implemented using the functional programming paradigm."
-
-# Class Design:
-# - Provides broad, higher-level calculations across multiple habits
-# - Doesn't maintain state of its own, but analyzes user/habit data
-# - Already uses Streaks functionality through habit.streaks references
 
 class Analytics:
-    """Analyzes the user's habits and provide statistics."""
+    """
+    Analyzes the user's habits and provide statistics using functional programming
+    across different dimensions like streaks, completions, and periodicity.
+
+    This class contains all analytics functionality required for the habit tracker.
+
+    Note: Most of the methods don't need to check no habits/completions logic,
+          because it is dealt with in the cli menus logic and db operations.
+
+    Attributes:
+        user(User): The user whose habits will be analyzed.
+    """
 
     def __init__(self, user: User) -> None:
         """
-        Initializes the DataAnalytics object.
+        Initializes the Analytics object with a user.
 
-        :param user: The User object whose habits will be analyzed.
+        Args:
+            user: The user object whose habits will be analyzed.
         """
-        self.user = user         # Store the user for analytics
+        self.user = user         # Store the user reference for analytics operations
 
     # Task requirement: "return a list of all currently tracked habits"
     def list_all_habits(self) -> List[Habit]:
         """
-        Retrieves the current habits of the user.
+        Retrieves all habits for the selected user.
 
-        :return: List of habit names.
+        Returns:
+            List[Habit]: List of all habits for the user.
         """
         # Using list comprehension
         # return self.user.habits
 
         # Using FPP
-        # Map extracts the "name" of each habit from self.user.habits and returns the result as a list
+        # Map extracts the identity of each habit from self.user.habits and returns the result as a list
         all_habits_list = list(map(lambda habit: habit, self.user.habits))
         return all_habits_list
 
     # Task requirement: "return a list of all habits with the same periodicity"
     def list_habits_by_periodicity(self, periodicity: str) -> List[Habit]:
         """
-        Retrieves habits filtered by the period of their completion (daily or weekly).
+        Retrieves habits filtered by a given periodicity.
 
-        :param periodicity: The frequency to filter habits by.
-        :return: List of habit names that match the given filter.
+        Args:
+            periodicity: The frequency to filter habits by ("daily" or "weekly).
+        Returns:
+            List[Habit]: List of habits matching the given periodicity.
         """
         # Using list comprehension
         # return [habit.name for habit in self.user.habits if habit.frequency == periodicity]
@@ -53,18 +61,15 @@ class Analytics:
         return filtered_habits
 
     # Task requirement: "return the longest streak of all defined habits"
-    def longest_streak_all_habits(self) -> tuple:
+    def longest_streak_all_habits(self) -> Tuple[str, int]:
         """
-        Retrieves the longest streak of all defined habits.
+        Finds the habit with the longest streak across all habits.
 
-        :return: A tuple (habit_name, longest_streak) of the habit with the longest streak.
+        Returns:
+            Tuple[str, int]: A tuple containing (habit_name, longest_streak).
         """
-        # Using FPP
-        # map() to iterate through the longest streak for each habit and compare the streaks (second element)
-        #   and use max() to get the highest value
-
-        # Don't need to check no habits logic
-        # It is dealt with in the menu_analytics_all_habits()
+        # Map each habit to a tuple of (name, longest_streak)
+        # Then use max() to find he tuple with the highest longest_streak value
         longest_streak_all_habits = max(
             map(lambda habit: (habit.name, habit.streaks.get_longest_streak()), self.user.habits),
             key=lambda x: x[1]
@@ -73,126 +78,137 @@ class Analytics:
         return longest_streak_all_habits[0], longest_streak_all_habits[1]
 
     # Task requirement: "return the longest streak for a given habit"
-    def longest_streak_for_habit(self, habit_name: str) -> tuple:
+    def longest_streak_for_habit(self, habit_name: str) -> Tuple[str, int]:
         """
-        Retrieves the longest streak for a given habit.
+        Finds the longest streak for a selected habit.
 
-        :param habit_name: The name of the habit to get the longest streak for.
-        :return: A tuple (habit_name, longest_streak) of the longest streak for a selected habit.
+        Args:
+            habit_name: The name of the habit to get the longest streak for.
+        Returns:
+            Tuple[str, int]: A tuple containing (habit_name, longest_streak).
         """
-        # Using FPP
-        # Use next() to get the first matching habit (or None if no match),
-        #   while iterating with filter() over self.user.habits applying the lambda function
-        habit = next(
-            filter(lambda habit_item: habit_item.name == habit_name, self.user.habits),
-            None
-        )
+        # Filter habits to find the one with the matching name
+        # Use next() to get the first (and only matching habit)
+        habit = next(filter(lambda habit_item: habit_item.name == habit_name, self.user.habits))
 
         return habit_name, habit.streaks.get_longest_streak()
 
-    def longest_streak_by_periodicity(self, periodicity: str) -> tuple:
+    def longest_streak_by_periodicity(self, periodicity: str) -> Tuple[str, int]:
         """
-        Returns the habit with the longest streak for a specific periodicity.
+        Finds the habit with the longest streak for a given periodicity.
 
-        :param periodicity: The periodicity to filter by ("daily" or "weekly").
-        :return: Tuple of (habit_name, longest_streak) or ("None", 0) if no habits.
+        Args:
+            periodicity: The periodicity to filter by ("daily" or "weekly").
+        Returns:
+            Tuple[str, int]: A tuple of (habit_name, longest_streak)
+                                     or ("None", 0) if no habit exists; not accounted for in the cli logic.
         """
         # Filter habits by periodicity
         habits = self.list_habits_by_periodicity(periodicity)
 
         # Check if no habits exist with chosen periodicity
+        # E.g.: user might have daily habits but no weekly yet
         if not habits:
             return "None", 0
 
-        # Else return the tuple with habit_name, longest_streak
+        # Find the habit with the maximum longest_streak
         max_habit = max(habits, key=lambda h: h.streaks.longest_streak)
         return max_habit.name, max_habit.streaks.longest_streak
 
-    def most_completed_habit(self) -> tuple:
+    def most_completed_habit(self) -> Tuple[str, int]:
         """
-        Finds the habits with the most completions.
+        Finds the habits with the most completions across all habit.
 
-        :return: A tuple (habit_name, completion_count) of the habit
+        Returns:
+             Tuple[str, int]: A tuple of (habit_name, completion_count).
         """
-        # Passing max() to the list of tuples created by iterating with map()
-        #   to find the tuple with the highest count of completions
-
+        # Map each habit to a tuple of (name, completion_count)
+        # Then find the tuple with the highest count
         most_completed = max(
             map(lambda habit: (habit.name, len(habit.completion_dates)), self.user.habits),
-            key=lambda x: x[1] # Sort by completion count
+            key=lambda x: x[1] # Sort by the 2nd element (completion count)
         )
 
         return most_completed
 
-    def most_completed_by_periodicity(self, periodicity: str) -> tuple:
+    def most_completed_by_periodicity(self, periodicity: str) -> Tuple[str, int]:
         """
-        Returns the habit with the most completions for a specific periodicity.
+        Finds the habit with the most completions for a given periodicity.
 
-        :param periodicity: The periodicity to filter by ("daily" or "weekly).
-        :return: Tuple of (habit_name, completion_count) or ("None", 0) if no habits.
+        Args:
+            periodicity: The periodicity to filter by ("daily" or "weekly")
+        Returns:
+            Tuple[str, int]: A tuple of (habit_name, completion_count)
+                                     or ("None", 0) if no habit exists; not accounted for in the cli logic.
         """
         # Filter habits by periodicity
         habits = self.list_habits_by_periodicity(periodicity)
 
+        # Check if no habit exists with chosen periodicity
         if not habits:
             return "None", 0
 
+        # Find the habit with the max number of completions
         max_habit = max(habits, key=lambda h: len(h.completion_dates))
         return max_habit.name, len(max_habit.completion_dates)
 
     def least_completed_habit(self) -> tuple:
         """
-        Finds the habit with the fewest completions.
+        Finds the habit with the fewest completions across all habits.
 
-        :return: A tuple (habit_name, completion_count) of the habit
+        Returns:
+            Tuple[str, int]: A tuple of (habit_name, completion_count).
         """
-        # Passing min() to the list of tuples created by iterating with map()
-        #   to find the tuple with the lowest count of completions
+        # Map each habit to a tuple of (name, completion_count)
+        # Then find the tuple with the lowest count
         least_completed = min(
             map(lambda habit: (habit.name, len(habit.completion_dates)), self.user.habits),
-            key=lambda x: x[1]
+            key=lambda x: x[1] # Sort by the 2nd element (completion_count)
         )
 
         return least_completed
 
 
-    def least_completed_by_periodicity(self, periodicity: str) -> tuple:
+    def least_completed_by_periodicity(self, periodicity: str) -> Tuple[str, int]:
         """
-        Returns the habit with the most completions for a specific periodicity.
+        Finds the habit with the fewest completions for a given periodicity.
 
-        :param periodicity: The periodicity to filter by ("daily" or "weekly).
-        :return: Tuple of (habit_name, completion_count) or ("None", 0) if no habits.
+        Args:
+            periodicity: The periodicity to filter by ("daily" or "weekly).
+        Returns:
+             Tuple[str, int]: A tuple of (habit_name, completion_count)
+                                      or ("None", 0) if no habit exists; not accounted for in the cli logic.
         """
         # Filter habits by periodicity
         habits = self.list_habits_by_periodicity(periodicity)
 
+
+        # Check if no habit exists with chosen periodicity
         if not habits:
             return "None", 0
 
+        # Find the habit with the minimum number of completions
         min_habit = min(habits, key=lambda h: len(h.completion_dates))
         return min_habit.name, len(min_habit.completion_dates)
 
     def average_streak_length_habit(self, habit_name: str) -> float:
         """
-        Calculates the average streak length for a given habit based on its recorded streak length history.
+        Calculates the average streak length for a selected habit.
 
-        :param habit_name: The name of the habit to analyze.
-        :return: The average streak length as a float.
+        Args:
+            habit_name: The name of the habit to analyze.
+        Returns:
+            float: The average streak length.
         """
-        # Don't need to check no habit logic
-        # Only Habit Details Menu uses this method: no habit = no menu
-
         # Get the streak length history string from the db
-        # Returns a comma separated string of string lengths
+        # Returns a comma separated string of streak lengths
         streak_history = self.user.db.load_broken_streak_length(habit_name)
 
-        # If no history exists
+        # If no history exists, return 0
         if not streak_history:
             return 0.0
 
-        # Convert the comma separated string into a list of integers
-        # map() applies int() to each element of the split string
-        # list() converts the map object to a list for further processing
+        # Convert the comma separated string into a list of integers using map
         streak_lengths = list(map(int, streak_history.split(",")))
 
         # Calculate the average
@@ -204,34 +220,34 @@ class Analytics:
         """
         Calculates the average streak length across all habits.
 
-        :return: Average streak as a float.
+        Returns:
+            float: The average streak as a float.
         """
-        # Don't need to check no habits logic
-        # It is dealt with in the menu_analytics_all_habits()
-
         # Initialize an empty list to collect streak lengths from all habits
         all_streak_lengths = []
 
-        # Iterate through all habits for the current user
+        # Process each habit to collect streak lengths
         for habit in self.user.habits:
-            # Retrieve streak history for each habit
             streak_history = self.user.db.load_broken_streak_length(habit.name)
 
             # If streak history exists, convert and add to the collection
             if streak_history:
+                # Use map to convert string values to integers
                 all_streak_lengths.extend(list(map(int, streak_history.split(","))))
 
-        # Calculate the average
+        # Calculate the average (handling the case of empty list)
         average_streak = sum(all_streak_lengths) / len(all_streak_lengths) if all_streak_lengths else 0.0
 
         return average_streak
 
     def average_streak_by_periodicity(self, periodicity: str) -> float:
         """
-        Calculates the average streak length for a specific periodicity based on streak history.
+        Calculates the average streak length for habits with a given periodicity.
 
-        :param periodicity: The periodicity to filter by ("daily" or "weekly")
-        :return: Average streak as a float.
+        Args:
+            periodicity: The periodicity to filter by ("daily" or "weekly")
+        Returns:
+            float: The average streak length.
         """
         # Filter habits by periodicity
         habits = self.list_habits_by_periodicity(periodicity)
@@ -239,7 +255,7 @@ class Analytics:
         # Initialize an empty list to collect streak lengths
         all_streak_lengths = []
 
-        # Iterate through habits with the specified periodicity
+        # Process each habit to collect streak lengths
         for habit in habits:
             streak_history = self.user.db.load_broken_streak_length(habit.name)
 
@@ -247,7 +263,7 @@ class Analytics:
             if streak_history:
                 all_streak_lengths.extend(list(map(int, streak_history.split(","))))
 
-        # Calculate the average
+        # Calculate the average (handling the case of empty list)
         average_streak = sum(all_streak_lengths) / len(all_streak_lengths) if all_streak_lengths else 0.0
 
         return average_streak
