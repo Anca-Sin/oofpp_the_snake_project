6f97861b7873d58de38c41c4889faf99a42674e9
Anca.Sin <Anca.Sin@gmail.com>
2025-03-07 16:05:54 +0100

    Review menu_my_habit_tracker - correct code - logic - documentation

--- cli/menu_my_habit_tracker.py
+++ cli/menu_my_habit_tracker.py
@@ -1,18 +1,23 @@
 """
-Menu: My Habit Tracker
+Menu: My Habit Tracker - Second after Main Menu
 """
-from cli.menu_analytics import menu_analytics
+from cli.menu_analytics import menu_analytics_all_habits
 from cli.menu_habits import menu_habits
 from helpers.helper_functions import reload_cli, reload_menu_countdown
-
 
 def menu_my_habit_tracker(ht):
     """
     Displays the habit tracker menu and handles user navigation.
 
-    :param ht:  The HabitTracker instance.
+    - redirects to habits manager (create, view, complete, delete)
+    - redirects to viewing analytics across all habits
+    - can go back to main menu
+
+    Args:
+        ht: The HabitTracker instance that manages the application state.
     """
     while True:
+        # Clear the screen and display the menu header
         reload_cli()
         print("""- - - My Habit Tracker
         
@@ -21,17 +26,23 @@ def menu_my_habit_tracker(ht):
         3. Back to Main Menu
         """)
 
+        # Get user choice
         choice = input("\nEnter your choice (1-3): ").strip()
 
+        # Handle menu options
         if choice == "1":
+            # Got to Habits Menu for managing habits
             menu_habits(ht)
 
         elif choice == "2":
-            menu_analytics(ht)
+            # Go to Analytics Menu across all habits
+            menu_analytics_all_habits(ht)
 
         elif choice == "3":
+            # Return to Main Menu
             return
 
         else:
+            # Handle invalid input
             print("\nInvalid input. Please try again!")
             reload_menu_countdown()
